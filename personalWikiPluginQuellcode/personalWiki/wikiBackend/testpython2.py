85407f3c38c73742a457b0386a68063174b416d1
andiandiandi <andy.libert@hotmail.de>
2022-03-14 18:03:19 +0100

    truly final

--- personalWikiPluginQuellcode/personalWiki/wikiBackend/testpython2.py
+++
@@ -1,9 +0,0 @@
-import os
-
-a = "C:\\Users\\Andre\\Desktop\\aWiki\\subfolder\\a.md"
-b = "C:\\Users\\Andre\\Desktop\\aWiki\\b.md"
-
-c = "C:\\Users\\Andre\\Desktop\\aWiki\\subfolder\\..\\test.md"
-d = " C:/Users/Andre/Desktop/aWiki/subfolder\\..\\test.md"
-
-print(os.path.normpath(d))
